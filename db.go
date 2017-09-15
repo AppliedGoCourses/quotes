cd9111ff0e9dccbcb3c4c83d564737abba306d0a
christophberger <christophberger@users.noreply.github.com>
2017-09-15 13:24:09 +0200

    WebServiceExercise: Add List() and Close(). Fix Get().

--- db.go
+++ db.go
@@ -25,6 +25,16 @@ func Open(path string) (*DB, error) {
 	}, nil
 }
 
+func (d *DB) Close() error {
+	err := d.db.Close()
+	if err != nil {
+		return errors.Wrap(err, "Close: cannot close database")
+	}
+	return nil
+}
+
+// Put takes a quote and saves it to the database, using the author name
+// as the key.
 func (d *DB) Put(q *Quote) error {
 	err := d.db.Update(func(tx *bolt.Tx) error {
 
@@ -36,7 +46,7 @@ func (d *DB) Put(q *Quote) error {
 		b, err := q.Serialize()
 		err = bucket.Put([]byte(q.Author), b)
 		if err != nil {
-			return errors.Wrapf(err, "Put: cannot put quote %d into bucket", q.ID)
+			return errors.Wrapf(err, "Put: cannot put quote from %s into bucket", q.Author)
 		}
 		return nil
 	})
@@ -47,10 +57,14 @@ func (d *DB) Put(q *Quote) error {
 	return nil
 }
 
+// Get takes an author name and retrieves the corresponding quote from the DB.
 func (d *DB) Get(author string) (*Quote, error) {
 	q := &Quote{}
 	err := d.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(quoteBucket))
+		if b == nil {
+			return errors.Errorf("Cannot get %s - there is no bucket named %s", author, quoteBucket)
+		}
 		v := b.Get([]byte(author))
 		err := q.Deserialize(v)
 		if err != nil {
@@ -62,4 +76,42 @@ func (d *DB) Get(author string) (*Quote, error) {
 		return nil, errors.Wrap(err, "Get: DB.View() failed")
 	}
 	return q, nil
+}
+
+// List lists all records in the DB.
+func (d *DB) List() ([]*Quote, error) {
+	// The database returns byte slices that we need to de-serialize
+	// into Quote structures.
+	structList := []*Quote{}
+
+	// We use a view as we don't update anything.
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(quoteBucket))
+		if b == nil {
+			// It is valid to attempt listing an empty database.
+			// Hence no error is returned in this case.
+			return nil
+		}
+		// ForEach iterates over all elements of a bucket and
+		// executes the passed-in function for each element.
+		err := b.ForEach(func(k []byte, v []byte) error {
+			q := &Quote{}
+			err := q.Deserialize(v)
+			if err != nil {
+				return errors.Wrapf(err, "List: cannot deserialize data of author %s", k)
+			}
+			// We're inside a closure, so we can access structList
+			// that resides in the outer scope.
+			structList = append(structList, q)
+			return nil
+		})
+		if err != nil {
+			return errors.Wrapf(err, "List: failed iterating over bucket %s", quoteBucket)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "List: view failed")
+	}
+	return structList, nil
 }
